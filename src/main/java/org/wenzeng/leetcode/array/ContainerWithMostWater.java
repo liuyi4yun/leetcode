75bc8e6af6d5856691266531e68f4b7cd5f514c1
wenzeng <wenzeng@gegejia.com>
2020-08-12 21:36:34 +0800

    Container With Most Water

--- src/main/java/org/wenzeng/leetcode/array/ContainerWithMostWater.java
+++ src/main/java/org/wenzeng/leetcode/array/ContainerWithMostWater.java
@@ -1,5 +1,7 @@
 package org.wenzeng.leetcode.array;
 
+import org.wenzeng.leetcode.Utils;
+
 /**
  * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
  * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，
@@ -19,4 +21,41 @@ package org.wenzeng.leetcode.array;
  * @version : ContainerWithMostWater.java, v 0.1 2020年08月11日 23:01:39 wenzeng Exp $
  */
 public class ContainerWithMostWater {
+
+    /**
+     * 问题，在数据类找到两个点,使得 min(heights[left],heights[right])*(right-left)最大
+     * <p>
+     * 双指针
+     */
+    public static Integer maxArea(int[] heights) {
+        if (heights == null || heights.length < 2) {
+            return null;
+        }
+        int left = 0, right = heights.length - 1;
+        int vol = vol(heights, left, right);
+        int max_vol = vol;
+        while (left < right) {
+            // 找出最大的
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+            vol = vol(heights, left, right);
+
+            if (vol > max_vol) {
+                max_vol = vol;
+            }
+        }
+        return max_vol;
+    }
+
+    public static int vol(int[] nums, int left, int right) {
+        return Math.min(nums[left], nums[right]) * (Math.abs(left - right));
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        Utils.print(maxArea(nums));
+    }
 }
